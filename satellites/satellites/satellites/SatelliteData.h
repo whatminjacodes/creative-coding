de69cd8b16c0f3970b85ea9cd024f64de0c9c94d
whatminjacodes <whatminjacodes@gmail.com>
2021-02-25 13:28:14 +0200

    Created new OpenFrameworks project

--- satellites/satellites/satellites/SatelliteData.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class SatelliteData {
-	/*
-	 *  Notes: Should maybe use python converter to convert TLE data to EFEC and only after that read the file
-	 */
-	const char* firstLine;		// Satellite dataset has two lines for each satellite
-	const char* secondLine;
-};
